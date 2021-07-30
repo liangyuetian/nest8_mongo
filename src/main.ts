8e5cc70b46205e427db52fc2472d254281f7c66d
王闯 <wangchuang@ymt360.com>
2021-07-30 18:02:49 +0800

    添加 热更新；webpack最高5.45.1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
+  console.log('服务开始启动...');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`服务开始启动成功: ${await app.getUrl()}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => {
+      console.log('服务关闭');
+      return app.close();
+    });
+  }
 }
+
 bootstrap();
